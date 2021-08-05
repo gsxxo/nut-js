bb555790ac0742979a7224e18ecf6f92615666fe
Kappa <59264215+banevasion@users.noreply.github.com>
2021-08-05 19:43:41 +0200

    Delete dist/lib/expect directory

--- dist/lib/expect/matchers/toBeAt.function.d.ts
+++
@@ -1,7 +0,0 @@
-import { Mouse } from "../../mouse.class";
-import { Point } from "../../point.class";
-export declare const toBeAt: (received: Mouse, position: Point) => Promise<{
-    message: () => string;
-    pass: boolean;
-}>;
-//# sourceMappingURL=toBeAt.function.d.ts.map
--- dist/lib/expect/matchers/toBeIn.function.d.ts
+++
@@ -1,7 +0,0 @@
-import { Mouse } from "../../mouse.class";
-import { Region } from "../../region.class";
-export declare const toBeIn: (received: Mouse, region: Region) => Promise<{
-    message: () => string;
-    pass: boolean;
-}>;
-//# sourceMappingURL=toBeIn.function.d.ts.map
